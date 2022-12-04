1d02a0a04f5a2f5f5d29a05446803c7c69a1bdcf
Jake <jakedevquestions@gmail.com>
2022-12-04 09:20:15 -0500

    added keyboard layout and some updates

--- waybar/scripts/weather.py
+++ waybar/scripts/weather.py
@@ -58,7 +58,7 @@ WEATHER_CODES = {
 data = {}
 
 
-weather = requests.get("https://wttr.in/Tampa, Florida?format=j1").json()
+weather = requests.get("https://wttr.in/47.203.67.52?format=j1").json()
 
 
 def format_time(time):
